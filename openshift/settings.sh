0b56db4fa21f061ecb3d0062caf651d44283e083
Mike Lyttle <mikelyttle@gmail.com>
2020-07-17 18:06:34 -0700

    MALS-242 Create OpenShift scripts to build and deploy app

---
+++ openshift/settings.sh
@@ -0,0 +1,23 @@
+# bash script to be sourced to set environment variables for OpenShift scripts
+
+export PROJECT_NAMESPACE="bsoszr"
+
+export GIT_URI="https://github.com/bcgov/nr-mals.git"
+export GIT_REF="master"
+
+# The templates that should not have their GIT references (uri and ref) over-ridden
+# Templates NOT in this list will have they GIT references over-ridden
+# with the values of GIT_URI and GIT_REF
+export -a skip_git_overrides=""
+
+# The project components
+export components=${components:-"app"}
+
+# The builds to be triggered after buildconfigs created (not auto-triggered)
+export builds=${builds:-""}
+
+# The images to be tagged after build
+export images=${images:-"mals-app"}
+
+# The routes for the project
+export routes=${routes:-""}
