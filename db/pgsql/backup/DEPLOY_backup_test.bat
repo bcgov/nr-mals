f38692a0874f985d1f7a94de3bba693c76d7b4f6
MScallion <62470743+MScallion@users.noreply.github.com>
2022-03-17 20:27:33 -0700

    MALS-1155: Corrected misaligned parameters and values in the backup templates.

--- db/pgsql/backup/DEPLOY_backup_test.bat
+++ db/pgsql/backup/DEPLOY_backup_test.bat
@@ -9,7 +9,7 @@ oc project 30245e-test
 oc -n 30245e-test create configmap backup-conf-mals --from-file=./config/backup.conf
 oc -n 30245e-test label configmap backup-conf-mals app=mals-bkup
 
-oc process --param-file=backup-deploy.test.param -f backup-deploy.yaml -n 30245e-test | oc create -f -
+oc process --param-file=backup-deploy_test.param -f backup-deploy.yaml -n 30245e-test | oc create -f -
  
 echo End of Script
 
