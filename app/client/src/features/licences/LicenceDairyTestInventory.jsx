03d5617c1b4a156521caac1910482ecde692ef36
Kyle Morel <kmorel@parcsystems.ca>
2023-03-29 20:02:47 -0700

    Resolve dairy test inv saving incorrect values

--- app/client/src/features/licences/LicenceDairyTestInventory.jsx
+++ app/client/src/features/licences/LicenceDairyTestInventory.jsx
@@ -44,7 +44,7 @@ export default function LicenceDairyTestInventory({ licence }) {
     reValidateMode: "onBlur",
   });
 
-  const { handleSubmit, setValue, register } = form;
+  const { handleSubmit, setValue, register, formState: { errors } } = form;
 
   useEffect(() => {
     dispatch(fetchLicenceDairyTestResults(licence.data.id));
@@ -147,12 +147,13 @@ export default function LicenceDairyTestInventory({ licence }) {
       obj.correspondence
     );
 
-    console.log(obj);
     setInventory([...inventory, obj]);
   }
 
   function resetInventory() {
+    setInitialInventory([]);
     setInventory([]);
+    dispatch(fetchLicenceDairyTestResults(licence.data.id));
   }
 
   function deleteRow(index) {
@@ -165,7 +166,7 @@ export default function LicenceDairyTestInventory({ licence }) {
       setValue(`inventory.${i}`, inventory[i + 1]);
     }
 
-    setValue(`inventory.${inventory.length}`, undefined);
+    setValue(`inventory.${inventory.length - 1}`, undefined);
 
     const clone = [...inventory];
     clone.splice(index, 1);
@@ -173,6 +174,7 @@ export default function LicenceDairyTestInventory({ licence }) {
   }
 
   const onSubmit = (data) => {
+    console.log(data);
     // Just make this array if it doesnt exist
     if (data.inventoryDates === undefined) {
       data.inventoryDates = [];
@@ -184,6 +186,7 @@ export default function LicenceDairyTestInventory({ licence }) {
         ...(data.inventoryDates[index] ?? { date: undefined }),
       };
     });
+
 
     // Take original object and add/overwrite new values
     let update = { ...dairyTestResults.data[0] };
@@ -276,6 +279,7 @@ export default function LicenceDairyTestInventory({ licence }) {
     item.value = parseAsFloat(value);
     clone[index] = item;
     setInventory([...clone]);
+    setValue(`inventory.${index}.value`, value);
   };
 
   const handleDateChange = (field, index) => {
@@ -444,12 +448,13 @@ export default function LicenceDairyTestInventory({ licence }) {
         <Col>
           <Form.Group controlId={`inventory.${index}.value`}>
             <Form.Control
-              type="text"
+              type="number"
               name={`inventory.${index}.value`}
               defaultValue={row.value}
-              {...register(`inventory.${index}.value`)}
+              {...register(`inventory.${index}.value`, { required: true })}
               onChange={(e) => handleValueChange(index, e.target.value)}
               onBlur={() => calculateAction(index)}
+              isInvalid={errors.inventory && errors.inventory[index]}
             />
           </Form.Group>
         </Col>
